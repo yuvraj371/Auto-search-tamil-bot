3d77b4f41913824cdee9d6b4e0490b24563726a8
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-08-26 21:00:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Ajay:tamil178@tamil.e8uxhjz.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Ajay")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://advance:<password>@aafilterbot.wfx10an.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "advance")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
