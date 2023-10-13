71840589a3f679d5624cb18fdce2c26e9253117b
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-10-13 14:03:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -401,7 +401,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = 'https://t.me/+Qn6fthcb7wI0ZTk1' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = 'https://t.me/+Qn6fthcb7wI0ZTk1 ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
@@ -422,7 +422,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = 'https://t.me/+Qn6fthcb7wI0ZTk1' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    title = 'https://t.me/+Qn6fthcb7wI0ZTk1 ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
     if CUSTOM_FILE_CAPTION:
